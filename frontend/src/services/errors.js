f7a688cbf8e0dec1d1c3a47d8fa4e6e3573b1d21
forgeronduweb <forgeronduweb@gmail.com>
2026-06-06 15:00:41 +0000

    Delete frontend directory

--- frontend/src/services/errors.js
+++
@@ -1,7 +0,0 @@
-export class AuthError extends Error {
-    constructor(message, statusCode) {
-        super(message);
-        this.statusCode = statusCode;
-        this.name = 'AuthError';
-    }
-}
